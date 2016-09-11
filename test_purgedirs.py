09f5d3a7a1fe2de4944a97f1f45ca1705f94f246
Daniel Hosterman <daniel.hosterman@gmail.com>
2016-09-11 22:12:49 +0000

    Add tests for cli

--- test_purgedirs.py
+++ test_purgedirs.py
@@ -1,18 +1,46 @@
+from functools import partial
 import os
+
+from click.testing import CliRunner
 import pytest
 
-from purgedirs import empty_directories
+from purgedirs import cli, empty_directories
 
 
 @pytest.fixture
-def os_walk_mock(*args, **kwargs):
-    return [
-        ("dir1", ["dir2", "dir3", "dir4"], ["file1"]),
-        ("dir5", ["dir6", "dir7", "dir8"], []),
-    ]
+def mock_os_walk(monkeypatch):
+    def mock_values(*args, **kwargs):
+        return [("dir1", ["dir2", "dir3", "dir4"], ["file1"]),
+                ("dir5", ["dir6", "dir7", "dir8"], []), ]
+
+    monkeypatch.setattr("os.walk", mock_values)
 
 
-def test_empty_directories(monkeypatch):
+@pytest.fixture
+def cli_runner():
+    runner = CliRunner()
+    return partial(CliRunner().invoke, cli)
+
+
+def test_empty_directories(mock_os_walk):
     """Should return the absolute path for directories without files"""
-    monkeypatch.setattr("os.walk", os_walk_mock)
     assert empty_directories(".") == [os.getcwd() + "/dir5"]
+
+
+@pytest.mark.parametrize("test_input", [(["--help"]), (["-h"])])
+def test_help_option(test_input):
+    result = cli_runner()(test_input)
+    assert "Show this message and exit" in result.output
+
+
+@pytest.mark.parametrize("test_input", [(["--dry-run", "."]), (["-d", "."])])
+def test_help_option(test_input, mock_os_walk):
+    result = cli_runner()(test_input)
+    assert "dir5" in result.output
+
+
+@pytest.mark.parametrize("test_input", [(["."]), ])
+def test_help_option(test_input, mock_os_walk):
+    result = cli_runner()(test_input)
+    assert "If this were real" in result.output
+    assert "dir5" in result.output
